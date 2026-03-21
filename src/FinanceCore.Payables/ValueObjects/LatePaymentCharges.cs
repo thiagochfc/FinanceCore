7bb1de0e32ec0f48f864264c36a9077c44ca2423
Thiago Christopher <thiago_cfc@hotmail.com>
2026-03-21 20:04:08 -0300

    feat(payables): add LatePaymentCharges

--- src/FinanceCore.Payables/ValueObjects/LatePaymentCharges.cs
+++ src/FinanceCore.Payables/ValueObjects/LatePaymentCharges.cs
@@ -1,6 +1,46 @@
+using FinanceCore.Shareds.Primitives;
+
 namespace FinanceCore.Payables.ValueObjects;
 
-public class LatePaymentCharges
+public sealed class LatePaymentCharges : ValueObject
 {
-    
+    private const int DaysPerMonth = 30;
+    public Percentage FinePercentage { get; }
+    public Percentage MonthlyInterestPercentage { get; }
+
+    private LatePaymentCharges(Percentage finePercentage, Percentage monthlyInterestPercentage)
+    {
+        FinePercentage = finePercentage;
+        MonthlyInterestPercentage = monthlyInterestPercentage;
+    }
+
+    public static LatePaymentCharges Create(Percentage finePercentage, Percentage monthlyInterestPercentage)
+    {
+        ArgumentNullException.ThrowIfNull(finePercentage);
+        ArgumentNullException.ThrowIfNull(monthlyInterestPercentage);
+
+        return new LatePaymentCharges(finePercentage, monthlyInterestPercentage);
+    }
+
+    public Money Calculate(Money money, int daysLate)
+    {
+        ArgumentNullException.ThrowIfNull(money);
+        ArgumentOutOfRangeException.ThrowIfNegative(daysLate);
+
+        if (daysLate == 0)
+        {
+            return Money.Zero(money.Currency);
+        }
+
+        Money fine = money.Multiply(FinePercentage.Calculation);
+        Money monthlyInterest =  money.Multiply(MonthlyInterestPerDay() * daysLate);
+        Money result = fine.Add(monthlyInterest);
+        return result;
+    }
+
+    private decimal MonthlyInterestPerDay() =>
+        MonthlyInterestPercentage.Calculation / DaysPerMonth;
+
+    protected override IReadOnlyList<object?> GetEqualityComponents() =>
+        [FinePercentage, MonthlyInterestPercentage];
 }
--- tests/FinanceCore.Payables.Tests/ValueObjects/LatePaymentChargesTests.cs
+++ tests/FinanceCore.Payables.Tests/ValueObjects/LatePaymentChargesTests.cs
@@ -1,6 +1,46 @@
+using System.Globalization;
+
+using FinanceCore.Payables.ValueObjects;
+
+using Shouldly;
+
 namespace FinanceCore.Payables.Tests.ValueObjects;
 
 public class LatePaymentChargesTests
 {
+    [Fact]
+    public void LatePaymentChargesShouldBeCreateSuccessfully()
+    {
+        // Arrange
+        Percentage finePercentage = Percentage.Create(0.00m);
+        Percentage monthlyInterestPercentage = Percentage.Create(0.00m);
+
+        // Act
+        LatePaymentCharges latePaymentCharges = LatePaymentCharges.Create(finePercentage, monthlyInterestPercentage);
+
+        // Assert
+        latePaymentCharges.FinePercentage.ShouldBe(finePercentage);
+        latePaymentCharges.MonthlyInterestPercentage.ShouldBe(monthlyInterestPercentage);
+    }
     
+    [Theory]
+    [InlineData("100.00", 0, "0.00")]
+    [InlineData("100.00", 1, "2.03")]
+    [InlineData("100.00", 10, "2.33")]
+    public void ShouldCalculateCorrectly(string amountInput, int daysLate, string latePaymentChargesInput)
+    {
+        // Arrange
+        Percentage finePercentage = Percentage.Create(2);
+        Percentage monthlyInterestPercentage = Percentage.Create(1);
+        LatePaymentCharges latePaymentCharges = LatePaymentCharges.Create(finePercentage, monthlyInterestPercentage);
+        Money money = Money.Create(decimal.Parse(amountInput, CultureInfo.InvariantCulture), Currency.BRL);
+        Money moneyExpected = Money.Create(decimal.Parse(latePaymentChargesInput, CultureInfo.InvariantCulture),
+            Currency.BRL);
+
+        // Act
+        var result = latePaymentCharges.Calculate(money, daysLate);
+
+        // Assert
+        result.ShouldBe(moneyExpected);
+    }
 }
