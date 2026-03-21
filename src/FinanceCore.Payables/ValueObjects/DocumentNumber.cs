0546e8fc74865326c0eddcee7446c7441cbecd6a
Thiago Christopher <thiago_cfc@hotmail.com>
2026-03-21 18:19:53 -0300

    feat(payables): add DocumentNumber

---
+++ src/FinanceCore.Payables/ValueObjects/DocumentNumber.cs
@@ -0,0 +1,50 @@
+using System.Text.RegularExpressions;
+
+using FinanceCore.Shareds.Errors;
+using FinanceCore.Shareds.Primitives;
+
+namespace FinanceCore.Payables.ValueObjects;
+
+public sealed partial class DocumentNumber : ValueObject
+{
+    [GeneratedRegex(@"\p{Cc}", RegexOptions.Compiled)]
+    private static partial Regex ControlCharsRegex();
+
+    private const int MaximumLength = 50;
+
+    public string Value { get; }
+
+    private DocumentNumber(string value) =>
+        Value = value;
+
+    public static DocumentNumber Create(string value)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+        var sanitized = ControlCharsRegex().Replace(value, "");
+
+        // Validating again, as it may be empty after cleaning.
+        ArgumentException.ThrowIfNullOrWhiteSpace(sanitized);
+        ThrowIfLengthIsTooLong(sanitized);
+
+        return new DocumentNumber(sanitized);
+    }
+
+    private static void ThrowIfLengthIsTooLong(string value)
+    {
+        if (value.Length <= MaximumLength)
+        {
+            return;
+        }
+
+        ContextError contextError = new("DocumentNumber.TooLong",
+            $"DocumentNumber is too long. Maximum allowed length is: {MaximumLength}");
+        throw new TooLongDocumentNumberException(contextError);
+    }
+
+    protected override IReadOnlyList<object?> GetEqualityComponents() =>
+        [Value];
+}
+
+#pragma warning disable CA1032
+public sealed class TooLongDocumentNumberException(ContextError contextError) : DomainException(contextError);
+#pragma warning restore CA1032
---
+++ tests/FinanceCore.Payables.Tests/ValueObjects/DocumentNumberTests.cs
@@ -0,0 +1,55 @@
+using FinanceCore.Payables.ValueObjects;
+
+using Shouldly;
+
+namespace FinanceCore.Payables.Tests.ValueObjects;
+
+public class DocumentNumberTests
+{
+    [Fact]
+    public void DocumentNumberShouldBeCreateSuccessfully()
+    {
+        // Arrange
+        const string value = "FAT001"; 
+        
+        // Act
+        DocumentNumber documentNumber = DocumentNumber.Create("FAT001");
+
+        // Assert
+        documentNumber.Value.ShouldBe(value);
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void DocumentNumberShouldNotBeCreatedDueToEmptyValue(string? value)
+    {
+        // Arrange
+        Should.Throw<ArgumentException>(() => DocumentNumber.Create(value!));
+    }
+
+    [Fact]
+    public void DocumentNumberShouldNotBeCreatedDueTooLong()
+    {
+        // Arrange
+        const string value = "123456789012345678901234567890123456789012345678901"; 
+        
+        // Assert
+        Should.Throw<TooLongDocumentNumberException>(() => DocumentNumber.Create(value));
+    }
+    
+    [Fact]
+    public void DocumentNumberShouldNotContainsControlChars()
+    {
+        // Arrange
+        const string value = "1\n2\t3\r4\0";
+        const string expected = "1234";
+        
+        // Act
+        DocumentNumber documentNumber = DocumentNumber.Create(value);
+        
+        // Assert
+        documentNumber.Value.ShouldBe(expected);
+    }
+}
